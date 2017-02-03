a9cd3dd1f0cd05e540a9be30898c13fe9a4db10e
Tanya2501 <tanya.bahmatovich@tut.by>
2017-02-03 16:45:28 +0300

    tic-tac-toe/feature: implement

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,4 +1,4 @@
-Class TicTacToe {
+class TicTacToe {
     constructor() {
 this.player='x';
 this.table=[];
@@ -22,27 +22,27 @@ return this.player;
     }
 
     isFinished() {
-        return this.getWinner()!==null || this.isDraw()===true || this.noMoreTurns()===true?true:false;   
+        return this.getWinner()!==null || this.isDraw()===true || this.noMoreTurns()===true?true:false;
 }
 
     getWinner() {
 for (var i=0; i<3;i++){
     var j=0;
-      if (this.table[i][j]==this.table[i][j+1] && this.table[i][j]==this.table[i][j+2] && this.table[i][j]!==null){  
+      if (this.table[i][j]==this.table[i][j+1] && this.table[i][j]==this.table[i][j+2] && this.table[i][j]!==null){
     return this.table[i][j];
 }
 }
 for (var j=0; j<3;j++){
    var i=0;
-    if (this.table[i][j]==this.table[i+1][j] && this.table[i][j]==this.table[i+2][j] && this.table[i][j]!==null) { 
+    if (this.table[i][j]==this.table[i+1][j] && this.table[i][j]==this.table[i+2][j] && this.table[i][j]!==null) {
     return this.table[i][j];
 }
-} 
+}
 var k=0;
 if (this.table[k][k]==this.table[k+1][k+1] && this.table[k][k]==this.table[k+2][k+2] && this.table[k][k]!==null){
     return this.table[k][k];
 }
-var p=0; 
+var p=0;
 var m=2;
 if (this.table[p][m]==this.table[p+1][m-1] && this.table[p][m]==this.table[p+2][m-2] && this.table[p][m]!==null){
     return this.table[p][m];
@@ -56,12 +56,12 @@ return null;
         if (this.table[j][i]===null){
             return false;
         }
-    }} 
+    }}
 return true;
 }
 
     isDraw() {
-        return this.noMoreTurns()===true && this.getWinner()===null; 
+        return this.noMoreTurns()===true && this.getWinner()===null;
 }
 
     getFieldValue(rowIndex, colIndex) {
